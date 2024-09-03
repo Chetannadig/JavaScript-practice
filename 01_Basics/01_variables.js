46be321ae8d5ea6085ac2119e5ee440ab4028803
Chetannadig <106951058+Chetannadig@users.noreply.github.com>
2024-09-03 12:27:49 +0000

    Till dataTypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=935350
+let accountEmail="chetannv108@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+console.log(accountId);
+
+accountEmail="chetan@g,ail.com"
+accountPassword="9353"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all code as newer version
+
+/*
+//datatypes
+number
+string
+boolean
+null
+undefined
+symbol
+object
+*/
